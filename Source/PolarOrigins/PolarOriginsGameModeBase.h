74d428605e8d9a0597974154e473602955077a0f
Ideasog01 <92227089+Ideasog01@users.noreply.github.com>
2022-12-12 10:57:34 +0000

    Initial Commit

---
+++ Source/PolarOrigins/PolarOriginsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PolarOriginsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POLARORIGINS_API APolarOriginsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
